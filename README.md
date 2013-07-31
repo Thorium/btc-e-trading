aee430ecec815e79280572b85852a5f5bb121f6e
duckmatt <matt@mattmcveigh.co.uk>
2013-07-31 16:35:23 +0000

    Starting to create the structure of the project

---
+++ README.md
@@ -0,0 +1,7 @@
+#F# Unaffiliated BTC-E Arbitrage Bot
+
+Just started development.
+
+###Design Goals
+
+- [ ] Calculate and automate arbitrage between currencies on the btc-e exchange.
---
+++ btc-e-bot/Arbitrage.fs
@@ -0,0 +1,25 @@
+﻿module Arbitrage
+
+open BtceApiFramework.Currency
+open BtceApiFramework.PublicBtceApi
+
+open System
+
+type Edge = { 
+    transactionFee: Decimal;
+    ask: Decimal;
+    sell: Decimal;
+    currencyPair: Pair
+}
+
+type Vertex = { 
+    currency: Currency;
+    edges: Edge list 
+}
+
+type Graph = { vertices: Vertex list }
+
+// Find shortest paths - shortest path = highest amount of money made; shortest path to the vertx you start from
+
+let findShortestPath (vertex: Vertex) : unit =
+    ()
