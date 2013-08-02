780823f38786e63d845320d2eacccb81862bb5d6
duckmatt <matt@mattmcveigh.co.uk>
2013-08-02 12:11:00 +0000

    Passed test for creating the arbitrage graph

---
+++ Tests/MockDownloader.fs
@@ -0,0 +1,53 @@
+﻿module MockDownloader
+
+let apiv3MockInfo = @"{
+    ""server_time"":1375428679,
+    ""pairs"":{
+        ""btc_usd"":{
+            ""decimal_places"":3,""min_price"":0.1,""max_price"":400,""min_amount"":0.01,""hidden"":0,""fee"":0.1
+        },
+        ""btc_eur"":{
+            ""decimal_places"":5,""min_price"":0.1,""max_price"":400,""min_amount"":0.1,""hidden"":0,""fee"":0.1
+        },
+        ""ltc_btc"":{
+            ""decimal_places"":5,""min_price"":0.0001,""max_price"":10,""min_amount"":0.1,""hidden"":0,""fee"":0.1
+        },
+        ""ltc_usd"":{
+            ""decimal_places"":6,""min_price"":0.0001,""max_price"":100,""min_amount"":0.1,""hidden"":0,""fee"":0.1
+        },
+        ""eur_usd"":{
+            ""decimal_places"":5,""min_price"":1,""max_price"":2,""min_amount"":0.1,""hidden"":0,""fee"":0.1
+        }
+    }
+}"
+
+let apiv3MockPriceQuotes = @"{
+	""btc_usd"":
+	{
+		""high"":96.969,""low"":93.211,""avg"":95.09,""vol"":410734.80816,""vol_cur"":4321.84986,""last"":95.45,""buy"":100,""sell"":90,""updated"":1375430811
+	},
+	""btc_eur"":
+	{
+		""high"":77.39499,""low"":73.9999,""avg"":75.697445,""vol"":16917.10877,""vol_cur"":222.59421,""last"":75,""buy"":100,""sell"":100,""updated"":1375430810
+	},
+	""ltc_btc"":
+	{
+		""high"":0.0288,""low"":0.0274,""avg"":0.0281,""vol"":2814.97356,""vol_cur"":100652.45131,""last"":0.02792,""buy"":10,""sell"":10,""updated"":1375430811
+	},
+	""ltc_usd"":
+	{
+		""high"":2.69,""low"":2.626,""avg"":2.658,""vol"":88361.82891,""vol_cur"":33231.11484,""last"":2.65,""buy"":10,""sell"":10,""updated"":1375430811
+	},
+	""eur_usd"":
+	{
+		""high"":1.2588,""low"":1.2469,""avg"":1.25285,""vol"":8780.35387,""vol_cur"":7021.91538,""last"":1.2571,""buy"":10,""sell"":10,""updated"":1375430811
+	}
+}"
+
+let mockDownloader (url: string) : string =
+    let uri = new System.Uri(url)
+    let apiMethod = uri.Segments.[3].ToLower().TrimEnd('/')
+    match apiMethod with
+        | "ticker" -> apiv3MockPriceQuotes
+        | "info" -> apiv3MockInfo
+        | _ -> failwith ("Unknown public api method: " + apiMethod)
--- Tests/TestArbitrage.fs
+++ Tests/TestArbitrage.fs
@@ -3,39 +3,50 @@
 open NUnit.Framework
 
 open BtceApiFramework.PrivateBtceApi
+open BtceApiFramework.Currency
 open BtceApiFramework
 
 open Arbitrage
+open MockDownloader
+
+let getVertexForCurrency (vertices: Vertex list) (currency: Currency) : Vertex =
+    List.find (fun vertex -> vertex.currency = currency) vertices
+
+let edgesArePairs (vertex: Vertex) (pairs: Pair list) : bool =
+    let matches = [ for pair in pairs do 
+                        yield List.exists (fun edge -> edge.currencyPair = pair) vertex.edges ]
+    not <| List.exists (fun m -> m = false) matches
 
 [<TestFixture>]
 type TestCurrency() = class
 
     [<Test>]
     member self.createArbitrageGraph() = 
-        let graph = createGraph PublicBtceApi.getInfo PublicBtceApi.getPriceQuotes 
+        let getInfo = fun() -> PublicBtceApi.getInfoWithCustomDownloader mockDownloader
 
-        let m = graph.vertices.Head.edges.Head
+        let getPriceQuotes = PublicBtceApi.getPriceQuotesWithCustomDownloader mockDownloader
 
-        System.Console.WriteLine(graph.vertices.Head.currency.ToString())
+        let graph = createGraph getInfo getPriceQuotes
 
-        System.Console.WriteLine(m.ask.ToString())
-        System.Console.WriteLine(m.sell.ToString())
-        System.Console.WriteLine(m.transactionFee.ToString())
-        System.Console.WriteLine(Currency.currencyPairToString(m.currencyPair))
+        let vertices = graph.vertices
 
-        let m = graph.vertices.Tail.Head.edges.Head
+        let btc = getVertexForCurrency vertices Currency.BTC
+        Assert.AreEqual(3, btc.edges.Length)
+        Assert.True(edgesArePairs btc [(Currency.BTC, Currency.USD);(Currency.BTC, Currency.EUR);(Currency.LTC, Currency.BTC)])
 
-        System.Console.WriteLine(graph.vertices.Tail.Head.currency.ToString())
+        let usd = getVertexForCurrency vertices Currency.USD
+        Assert.AreEqual(3, usd.edges.Length)
+        Assert.True(edgesArePairs usd [(Currency.BTC, Currency.USD);(Currency.LTC, Currency.USD);(Currency.EUR, Currency.USD)])
 
-        System.Console.WriteLine(m.ask.ToString())
-        System.Console.WriteLine(m.sell.ToString())
-        System.Console.WriteLine(m.transactionFee.ToString())
-        System.Console.WriteLine(Currency.currencyPairToString(m.currencyPair))
+        let eur = getVertexForCurrency vertices Currency.EUR
+        Assert.AreEqual(2, eur.edges.Length)
+        Assert.True(edgesArePairs eur [(Currency.EUR, Currency.USD);(Currency.BTC, Currency.EUR)])
 
-        let m = graph.vertices.Tail.Head.edges.Tail.Head
+        let ltc = getVertexForCurrency vertices Currency.LTC
+        Assert.AreEqual(2, ltc.edges.Length)
+        Assert.True(edgesArePairs ltc [(Currency.LTC, Currency.USD);(Currency.LTC, Currency.BTC)])
 
-        System.Console.WriteLine(m.ask.ToString())
-        System.Console.WriteLine(m.sell.ToString())
-        System.Console.WriteLine(m.transactionFee.ToString())
-        System.Console.WriteLine(Currency.currencyPairToString(m.currencyPair))
+    [<Test>]
+    member self.getShortestPath() = 
+        Assert.Fail()
 end
